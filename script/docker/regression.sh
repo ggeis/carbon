3268bcc5b8453c8eba493176d002c689a76cf8c7
Ihor Dluhosh <idluhosh@pgs-soft.com>
2020-04-03 12:16:23 +0200

    ci: script update

--- script/docker/regression.sh
+++ script/docker/regression.sh
@@ -26,7 +26,11 @@ done
 
 if $RUN_CHROME; then
   echo 'Running regression using chrome'
-  docker-compose exec -T cypress bash -c "./node_modules/.bin/cypress run --env CI=true --browser chrome --headless --reporter mocha-allure-reporter; npm run generate-cypress-allure-report"
+  docker-compose exec -T cypress bash -c "./node_modules/.bin/cypress run  './cypress/features/regression/a*.feature', './cypress/features/regression/b*.feature', './cypress/features/regression/c*.feature', './cypress/features/regression/d*.feature', './cypress/features/regression/e*.feature', './cypress/features/regression/f*.feature', './cypress/features/regression/g*.feature', './cypress/features/regression/h*.feature', './cypress/features/regression/i*.feature' --env CI=true --browser chrome --headless --reporter mocha-allure-reporter"
+  docker-compose exec -T cypress bash -c "./node_modules/.bin/cypress run  './cypress/features/regression/k*.feature', './cypress/features/regression/l*.feature', './cypress/features/regression/m*.feature', './cypress/features/regression/n*.feature', './cypress/features/regression/o*.feature', './cypress/features/regression/p*.feature', './cypress/features/regression/q*.feature', './cypress/features/regression/r*.feature', './cypress/features/regression/s*.feature' --env CI=true --browser chrome --headless --reporter mocha-allure-reporter"
+  docker-compose exec -T cypress bash -c "./node_modules/.bin/cypress run  './cypress/features/regression/t*.feature', './cypress/features/regression/u*.feature', './cypress/features/regression/v*.feature', './cypress/features/regression/w*.feature', './cypress/features/regression/x*.feature', './cypress/features/regression/y*.feature', './cypress/features/regression/z*.feature', './cypress/features/regression/test/*.feature', './cypress/features/regression/validations/*.feature' --env CI=true --browser chrome --headless --reporter mocha-allure-reporter"
+  docker-compose exec -T cypress bash -c "./node_modules/.bin/cypress run  './cypress/features/regression/deprecated/*.feature', './cypress/features/regression/experimental/*.feature', './cypress/features/themes/themes.feature' --env CI=true --browser chrome --headless --reporter mocha-allure-reporter"
+  docker-compose exec -T cypress bash -c "npm run generate-cypress-allure-report"
 fi
 
 if $RUN_FIREFOX; then
